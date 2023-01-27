30aa677492d59cefb77f146669330c7d0969aafe
Egor <aldes@list.ru>
2023-01-28 00:52:36 +0300

    скорректированы ошибки

--- task/Program.cs
+++ task/Program.cs
@@ -1,6 +1,6 @@
 ﻿string [] Array = {"hellow", "my", "friend", "how", "are", "you?"};
 string [] finalArray = new string[6];
-System.Console.Write("Строки, длина котортых мельше или равна 3: ");
+System.Console.Write("Строки, длина которых меньше или ровна 3: ");
 for (int i = 0; i < Array.Length; i++) 
 {
     if (Array[i].Length <= 3) 
