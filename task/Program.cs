70c1e09ac0f2d7c69677e9e6b673550665808b97
Egor <aldes@list.ru>
2023-01-28 00:15:22 +0300

    добавлена блок-схема(доработана)

--- task/Program.cs
+++ task/Program.cs
@@ -1,27 +1,11 @@
-﻿string [] Array = {"hellow", "my", "frind", "how", "are", "you?"};
-
-void ShowArray(string[] array) {
-    for (int i = 0; i < array.Length; i++) {
-        System.Console.Write($"{array[i]}  ");
-    }
-    System.Console.WriteLine();
-}
-
-string[] CheckArray(string[] array) {   
-    int count = 0;
-    int index = 0;
-    for (int i = 0; i < array.Length; i++) {
-        if (array[i].Length <= 3) count++;
-    }
-    string[] finalArray = new string[count];
-    for (int j = 0; j < array.Length; j++) {
-        if (array[j].Length <= 3) {
-            finalArray[index] = array[j];
-            index++;
-        }
-    }
-    return finalArray;
-}
-
+﻿string [] Array = {"hellow", "my", "friend", "how", "are", "you?"};
+string [] finalArray = new string[6];
 System.Console.Write("Строки, длина котортых мельше или равна 3: ");
-ShowArray(CheckArray(Array));
+for (int i = 0; i < Array.Length; i++) 
+{
+    if (Array[i].Length <= 3) 
+    {
+        finalArray[i] = Array[i];
+        Console.Write(finalArray[i] + ", ");
+    }
+}
